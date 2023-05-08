3951b3e117870853b1354a8130c1d04850754b0a
karkikusal <karkikusal16@gmail.com>
2023-05-08 20:58:24 +0545

    game

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        GamePanel gPanel = new GamePanel();
+        JFrame frame = new JFrame();
+        frame.setLocation(300,200);
+        frame.setSize(700,600);
+        frame.setTitle("Game");
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.add(gPanel);
+        frame.setVisible(true);
+
+
+
+
+    }
+}
